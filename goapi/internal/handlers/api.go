4e860413230b559d70f912ba0167715216056a6a
Preston Matson <71800110+prm20@users.noreply.github.com>
2025-12-09 01:32:57 -0800

    Add files via upload

---
+++ goapi/internal/handlers/api.go
@@ -0,0 +1,20 @@
+package handlers
+
+import (
+	"github.com/go-chi/chi"
+	chimiddle "github.com/go-chi/chi/middleware"
+	"github.com/avukadin/goapi/internal/middleware"
+)
+
+func Handler(r *chi.Mux) {
+	// global middleware
+	r.Use(chimiddle.StripSlashes)
+	r.Route("/account", func(router chi.Router) {
+		// middleware for /account route
+		router.Use(middleware.Authorization)
+
+		route.Get("/coins", GetCoinBalance)
+	})
+
+}
+
